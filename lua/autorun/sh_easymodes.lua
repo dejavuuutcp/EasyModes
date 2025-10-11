39f2d0472e8b2b361a14da8f71f3d8841ba31716
Misha Tsvetkov <102579612+dejavuuutcp@users.noreply.github.com>
2025-10-11 15:19:32 +0500

    Add files via upload

---
+++ lua/autorun/sh_easymodes.lua
@@ -0,0 +1,50 @@
+EasyModes = EasyModes or {}
+EasyModes.MODE_PVP = 2
+EasyModes.MODE_BUILD = 1
+EasyModes.NetworkString = "EasyModes"
+
+if SERVER then
+    util.AddNetworkString(EasyModes.NetworkString)
+    resource.AddFile("resource/fonts/Montserrat.ttf")
+end
+
+local FindMetaTable = FindMetaTable
+local hook_Add = hook.Add
+local IsValid = IsValid
+
+local PLAYER = FindMetaTable("Player")
+
+function PLAYER:GetMode()
+    return self:GetNWInt("PlayerMode", EasyModes.MODE_PVP)
+end
+
+function PLAYER:BuildMode()
+    return self:GetMode() == EasyModes.MODE_BUILD
+end
+
+function PLAYER:PVPMode()
+    return self:GetMode() == EasyModes.MODE_PVP
+end
+
+hook_Add("ScalePlayerDamage", "EasyModesScaleDamage", function(target, hitgroup, dmgInfo)
+    if target:BuildMode() then
+        return true
+    end
+
+    local attacker = dmgInfo:GetAttacker()
+    if IsValid(attacker) and attacker:IsPlayer() and attacker:BuildMode() then
+        return true
+    end
+
+    local inflictor = dmgInfo:GetInflictor()
+    if IsValid(inflictor) then
+        if inflictor:IsPlayer() and inflictor:BuildMode() then
+            return true
+        end
+
+        local owner = inflictor:GetOwner()
+        if IsValid(owner) and owner:IsPlayer() and owner:BuildMode() then
+            return true
+        end
+    end
+end)
