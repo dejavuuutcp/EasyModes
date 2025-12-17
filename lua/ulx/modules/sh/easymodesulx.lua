47efb56af3f8956cc30c5939f579e36e666a586f
Misha Tsvetkov <102579612+dejavuuutcp@users.noreply.github.com>
2025-12-17 23:00:08 +0500

    structure

--- lua/ulx/modules/sh/easymodesulx.lua
+++ lua/ulx/modules/sh/easymodesulx.lua
@@ -13,80 +13,80 @@ local ulx_command = ulx.command
 local UL_ACCESS_ALL = ULib.ACCESS_ALL
 local UL_ACCESS_SUPERADMIN = ULib.ACCESS_SUPERADMIN
 
-function ulx.build(calling_ply)
-    if not calling_ply:SetGameMode(EasyModes.MODE_BUILD) then
+local function switchMode(calling_ply, mode, modeName)
+    if not calling_ply:SetGameMode(mode) then
         ULib_tsayError(calling_ply, "Please wait before switching modes again", true)
         return
     end
 end
 
-local buildCmd = ulx_command(CATEGORY, "ulx build", ulx.build, "!build")
-buildCmd:defaultAccess(UL_ACCESS_ALL)
-buildCmd:help([[
-Switches your mode to BUILD mode.
-• You cannot take or deal any damage.
-• Noclip is enabled (if allowed in the config).
-• Your props and constructions are protected from damage.
-• Ideal for building safely without PvP interruptions.
-]])
+function ulx.build(calling_ply)
+    switchMode(calling_ply, EasyModes.MODE_BUILD, "BUILD")
+end
 
 function ulx.pvp(calling_ply)
-    if not calling_ply:SetGameMode(EasyModes.MODE_PVP) then
-        ULib_tsayError(calling_ply, "Please wait before switching modes again", true)
-        return
-    end
+    switchMode(calling_ply, EasyModes.MODE_PVP, "PVP")
 end
-
-local pvpCmd = ulx_command(CATEGORY, "ulx pvp", ulx.pvp, "!pvp")
-pvpCmd:defaultAccess(UL_ACCESS_ALL)
-pvpCmd:help([[
-Switches your mode to PVP mode.
-• You can deal and receive damage from other players.
-• Noclip and building protections are disabled.
-• Suitable for PvP.
-]])
 
 function ulx.setcooldown(calling_ply, seconds)
     RunConsoleCommand("easymodes_cooldown", tostring(seconds))
     ulx.fancyLogAdmin(calling_ply, "#A set EasyModes cooldown to #i second(s)", seconds)
 end
 
-local cooldownCmd = ulx_command(CATEGORY, "ulx setcooldown", ulx.setcooldown, "!setcooldown")
-cooldownCmd:addParam{ type = ULib.cmds.NumArg, min = 0, max = 1000, hint = "seconds" }
-cooldownCmd:defaultAccess(UL_ACCESS_SUPERADMIN)
-cooldownCmd:help([[
-Sets the cooldown (in seconds) between mode switches.
-Default: 1 second.
-]])
+local function toggleConVar(calling_ply, convarName, messageEnabled, messageDisabled)
+    local cvar = GetConVar(convarName)
+    local newValue = not cvar:GetBool()
+    RunConsoleCommand(convarName, newValue and "1" or "0")
+
+    if newValue then
+        ulx.fancyLogAdmin(calling_ply, messageEnabled)
+    else
+        ulx.fancyLogAdmin(calling_ply, messageDisabled)
+    end
+end
 
 function ulx.toggle_buildnoclip(calling_ply)
-    local cvar = GetConVar("easymodes_build_noclip")
-    local newValue = not cvar:GetBool()
-    RunConsoleCommand("easymodes_build_noclip", newValue and "1" or "0")
-
-    if newValue then
-        ulx.fancyLogAdmin(calling_ply, "#A enabled noclip in BUILD mode")
-    else
-        ulx.fancyLogAdmin(calling_ply, "#A disabled noclip in BUILD mode")
-    end
+    toggleConVar(
+        calling_ply, 
+        "easymodes_build_noclip", 
+        "#A enabled noclip in BUILD mode", 
+        "#A disabled noclip in BUILD mode"
+    )
 end
-
-local buildNoclipCmd = ulx_command(CATEGORY, "ulx buildnoclip", ulx.toggle_buildnoclip, "!buildnoclip")
-buildNoclipCmd:defaultAccess(UL_ACCESS_SUPERADMIN)
-buildNoclipCmd:help("Toggles noclip in BUILD mode")
 
 function ulx.toggle_respawnpvp(calling_ply)
-    local cvar = GetConVar("easymodes_respawn_on_pvp")
-    local newValue = not cvar:GetBool()
-    RunConsoleCommand("easymodes_respawn_on_pvp", newValue and "1" or "0")
-
-    if newValue then
-        ulx.fancyLogAdmin(calling_ply, "#A enabled auto-respawn on switching to PVP mode")
-    else
-        ulx.fancyLogAdmin(calling_ply, "#A disabled auto-respawn on switching to PVP mode")
-    end
+    toggleConVar(
+        calling_ply,
+        "easymodes_respawn_on_pvp",
+        "#A enabled auto-respawn on switching to PVP mode",
+        "#A disabled auto-respawn on switching to PVP mode"
+    )
 end
 
-local respawnPVPcmd = ulx_command(CATEGORY, "ulx respawnpvp", ulx.toggle_respawnpvp, "!respawnpvp")
-respawnPVPcmd:defaultAccess(UL_ACCESS_SUPERADMIN)
-respawnPVPcmd:help("Toggles auto-respawn when switching to PVP mode")
+local commands = {
+    {func = ulx.build,       cmd = "ulx build",       chat = "!build",       access = UL_ACCESS_ALL,        help = [[Switches your mode to BUILD mode.
+• You cannot take or deal any damage.
+• Noclip is enabled (if allowed in the config).
+• Your props and constructions are protected from damage.
+• Ideal for building safely without PvP interruptions.]]},
+
+    {func = ulx.pvp,         cmd = "ulx pvp",         chat = "!pvp",         access = UL_ACCESS_ALL,        help = [[Switches your mode to PVP mode.
+• You can deal and receive damage from other players.
+• Noclip and building protections are disabled.
+• Suitable for PvP.]]},
+
+    {func = ulx.setcooldown, cmd = "ulx setcooldown", chat = "!setcooldown", access = UL_ACCESS_SUPERADMIN, help = [[Sets the cooldown (in seconds) between mode switches. Default: 1 second.]], params = { type = ULib.cmds.NumArg, min = 0, max = 1000, hint = "seconds" }},
+
+    {func = ulx.toggle_buildnoclip, cmd = "ulx buildnoclip", chat = "!buildnoclip", access = UL_ACCESS_SUPERADMIN, help = "Toggles noclip in BUILD mode"},
+
+    {func = ulx.toggle_respawnpvp, cmd = "ulx respawnpvp", chat = "!respawnpvp", access = UL_ACCESS_SUPERADMIN, help = "Toggles auto-respawn when switching to PVP mode"}
+}
+
+for _, data in ipairs(commands) do
+    local c = ulx_command(CATEGORY, data.cmd, data.func, data.chat)
+    c:defaultAccess(data.access)
+    c:help(data.help)
+    if data.params then
+        c:addParam(data.params)
+    end
+end
